948f97235e72cd6372d563f90bdd29264465d753
Mao <139264586+ph071096@users.noreply.github.com>
2025-09-22 13:28:55 -0300

    save

--- prova_aula04_FOR.py
+++ prova_aula04_FOR.py
@@ -2,6 +2,7 @@
 # O programa deve solicitar ao usuário que insira dois números inteiros, representando o início e o fim do intervalo (inclusive).
 # Utilize um loop 'for' para iterar sobre todos os números no intervalo e somar apenas os números pares. Implemente a estrutura 'else' para exibir uma mensagem indicando que não há números pares no intervalo, caso seja o caso.
 # Ao final, exiba a soma dos números pares encontrados.
+
 intervalo1 = int(input('Digite o primeiro intervalo: '))
 intervalo2 = int(input('Digite o segundo intervalo: ')) + 1
 pares = 0
